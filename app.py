527b2ab9be54a8d70553b9a00a1dd1568959e1e3
Jessica <jessica.d.costa@hotmail.com>
2025-07-23 12:15:12 +0100

    first try

--- app.py
+++ app.py
@@ -4,37 +4,37 @@ from datetime import datetime
 def obter_resposta(texto: str) -> str:
     comando: str = texto.lower()
 
-    if comando in ('olá', 'boa tarde', 'bom dia'):
-        return 'Olá tudo bem!'
-    if comando == 'como estás':
-        return 'Estou bem, obrigado!'
-    if comando == 'como te chamas?':
-        return 'O meu nome é: Bot :)'
-    if comando == 'tempo':
-        return 'Está um dia de sol!'
-    if comando in ('bye', 'adeus', 'tchau'):
-        return 'Gostei de falar contigo! Até breve...'
-    if 'horas' in comando:
-        return f'São: {datetime.now():%H:%M} horas'
-    if 'data' in comando:
-        return f'Hoje é dia: {datetime.now():%d-%m-%Y}'
+    # if comando in ('olá', 'boa tarde', 'bom dia'):
+    #     return 'Olá tudo bem!'
+    # if comando == 'como estás':
+    #     return 'Estou bem, obrigado!'
+    # if comando == 'como te chamas?':
+    #     return 'O meu nome é: Bot :)'
+    # if comando == 'tempo':
+    #     return 'Está um dia de sol!'
+    # if comando in ('bye', 'adeus', 'tchau'):
+    #     return 'Gostei de falar contigo! Até breve...'
+    # if 'horas' in comando:
+    #     return f'São: {datetime.now():%H:%M} horas'
+    # if 'data' in comando:
+    #     return f'Hoje é dia: {datetime.now():%d-%m-%Y}'
 
     return f'Desculpa, não entendi a questão! {texto}'
 
-    # respostas = {
-    #     ('olá', 'boa tarde', 'bom dia'): 'Olá tudo bem!',
-    #     'como estás': 'Estou bem, obrigado!',
-    #     ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...',
-    # }
+    respostas = {
+        ('olá', 'boa tarde', 'bom dia'): 'Olá tudo bem!',
+        'como estás': 'Estou bem, obrigado!',
+        ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...',
+    }
 
-    # for chave, resposta in respostas.items():
-    #     if isinstance(chave, tuple):
-    #         if comando in chave:
-    #             return resposta
-    #     elif chave in comando:
-    #         return resposta
+    for chave, resposta in respostas.items():
+        if isinstance(chave, tuple):
+            if comando in chave:
+                return resposta
+        elif chave in comando:
+            return resposta
 
-    # return f'Desculpa, não entendi a questão! {texto}'
+    return f'Desculpa, não entendi a questão! {texto}'
 
 
 def chat() -> None:
@@ -43,14 +43,15 @@ def chat() -> None:
     name: str = input('Bot: Como te chamas? ')
     print(f'Bot: Olá, {name}! \n Como te posso ajudar?')
 
-    while True:
+while True:
         user_input: str = input('Tu: ')
-
+        resposta = obter_resposta(user_input)
+        print(f'Bot: {resposta}')
         if resposta == 'Gostei de falar contigo! Até breve...':
             break
 
-    print('Chat acabou')
-    print()
+        print('Chat acabou')
+        print()
 
 
 def main() -> None:
